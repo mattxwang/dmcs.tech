31a8da92029618c6709b96c979d94a58b24c3b47
Jack Sarick <jacksarick@gmail.com>
2016-09-14 17:11:20 -0400

    Save Wed 14 Sep 2016 17:11:20 EDT

---
+++ students/checkin.php
@@ -0,0 +1,20 @@
+<?php
+	include_once("../common.php");
+
+	$email = htmlspecialchars($_POST['email'], ENT_QUOTES);
+	$code  = htmlspecialchars($_POST['code'],  ENT_QUOTES);
+	$date  = date('y-m-d');
+	
+	$query = "INSERT INTO robotics.attendance VALUES ('$email', '$date', NULL);";
+
+	// execute query
+	$result = mysql_query($query) or die ("Error in query: ".mysql_error());
+	
+	// free result set memory
+	mysql_free_result($result);
+	
+	// close connection
+	mysql_close($connection);
+
+	header('Location: index.html?checkedin=true');
+?>
