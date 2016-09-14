55a4369b894a6a217a8e97f96f983feb13dda815
Jack Sarick <jacksarick@gmail.com>
2016-09-14 17:36:20 -0400

    Save Wed 14 Sep 2016 17:36:20 EDT

--- students/checkin.php
+++ students/checkin.php
@@ -5,7 +5,7 @@
 	$code  = htmlspecialchars($_POST['code'],  ENT_QUOTES);
 	$date  = date('y-m-d');
 	
-	$query = "INSERT INTO robotics.attendance VALUES ('$email', '$date', NULL);";
+	$query = "INSERT INTO robotics.attendance VALUES ('$email', '$date', NULL) WHERE EXISTS (SELECT * FROM robotics.attendance WHERE id=1 and date='swag');";
 
 	// execute query
 	$result = mysql_query($query) or die ("Error in query: ".mysql_error());
