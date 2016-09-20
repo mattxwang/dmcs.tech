4f51d2b5061b9d3bb5caffc801ef486223fba15c
Matthew Wang <matthewang.wang@gmail.com>
2016-09-19 23:43:09 -0400

    gets ready for final production

--- students/check-in.php
+++ students/check-in.php
@@ -3,22 +3,22 @@ layout: default
 ---
 
 <div class="container">
-	<?php if ($_GET["failed"]): ?>
+	<?php if ($_GET["failed"]){ ?>
 	<div class="alert alert-danger" role="alert">
 		<strong>Oh No!</strong> Wrong code, please try again.
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="this.parentElement.style.display = 'none';"><span aria-hidden="true">&times;</span></button>
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 	</div>
-	<?php endif; ?>
+	<?php } ?>
 	  <h1 class="page-heading">Check in</h1>
 	  <div class="row">
 	  	<div class="col-md-4 col-sm-12">
 		  <form action="checkin.php" method="post">
 		    <label alt="Your UCC email">Email: </label><input class="form-control" type="email" name="email">
-		    
+
 		    <br>
 
 		    <label alt="The code on the board">Code: </label><input class="form-control" type="text" name="code">
-		    
+
 		    <br>
 
 		    <button class="btn btn-primary" type="submit">Submit</button>
