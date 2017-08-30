33d4ccb5b646d723b7a87610c0aa6d123ae14219
Matthew Wang <matthewang.wang@gmail.com>
2017-08-30 14:47:13 -0400

    adds some more flair to the page + cleanup

--- students/index.php
+++ students/index.php
@@ -26,39 +26,16 @@ title: DMCS Club
         <div class="col-sm-4">
             <div class="well">
             <h2>Next Event:</h2>
-            <h3>Club meeting this Tuesday (September 20)</h3>
-            <h3>Fill out <a href="https://malsf21.typeform.com/to/l0PaV6">this survey</a></h3>
+            <p>Club hasn't started yet; wait until clubs fair!</p>
           </div>
         </div>
-        <!-- <div class="col-sm-4">
+        <div class="col-sm-4">
           <div class="well">
             <h2>Want more problems?</h2>
             <p>
               Try out some problems at:
               <div class="text-center"><a href="challenger" class="btn btn-lg btn-primary">Challenger</a></div>
             </p>
-          </div>
-        </div> -->
-        <div class="col-sm-4">
-          <div class="well">
-            <h2>Get Node Here:</h2>
-            <p>
-              Click on the button to download Node.js
-              <div class="text-center"><a href="https://nodejs.org/dist/v4.5.0/node-v4.5.0.pkg" class="btn btn-lg btn-primary">Node.js</a></div>
-            </p>
-          </div>
-        </div>
-        
-        <div class="col-sm-4">
-          <div class="panel panel-primary">
-            <div class="panel-heading">
-              <h2>Latest News</h2>
-            </div>
-            <div class="panel-body">
-              {% for post in site.posts limit: 5 %}
-                <h3><a href="{{ post.url | prepend: site.baseurl }}">{{ post.title }} <small>{{ post.date | date: "%b %-d, %Y" }}</small></a></h3>
-              {% endfor %}
-            </div>
           </div>
         </div>
       </div>
